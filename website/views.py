26e39e2278aa32f122b125d770b9aa8dfa61adcc
jfkeci <ja.lip132@gmail.com>
2021-08-05 13:21:56 +0200

    deleting a note

--- website/views.py
+++ website/views.py
@@ -1,10 +1,12 @@
 
+from flask.json import jsonify
 from website.models import Note
 from flask import Blueprint, request
 from flask.helpers import flash
 from flask.templating import render_template
 from flask_login import login_required, current_user
 from . import db
+import json
 
 views = Blueprint('views', __name__)
 
@@ -22,3 +24,16 @@ def home():
             db.session.commit()
             flash('Note added', category = 'success')
     return render_template("home.html", user = current_user)
+
+
+@views.route('/delete-note', methods = ['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    
+    return jsonify({})
