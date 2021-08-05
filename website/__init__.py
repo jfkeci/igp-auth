0d54af31f94dad973a09805e6dcd900404872b11
jfkeci <ja.lip132@gmail.com>
2021-08-05 12:55:02 +0200

    sign-up and login user

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager, login_manager
 
 db = SQLAlchemy()
  
@@ -8,7 +9,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'fdksljfačiiouoipfdus8879'
+    app.config['SECRET_KEY'] = 'fdksljfaiiouoipfdus8879'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
@@ -20,6 +21,14 @@ def create_app():
 
     from .models import User, Note
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     create_database(app)
 
     return app
@@ -27,4 +36,4 @@ def create_app():
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
         db.create_all(app = app)
-        print('Database created')
+        print('Database created!')
