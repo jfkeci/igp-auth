27f3b21122e0965b5f209ca198babf306fe347d1
jfkeci <ja.lip132@gmail.com>
2023-10-11 13:21:58 +0200

    feat: use HttpStatus enum in HttpException

--- src/utils/classes/http-exception.class.ts
+++ src/utils/classes/http-exception.class.ts
@@ -1,8 +1,10 @@
+import { HttpStatus } from '../enums/http-status.enum';
+
 export class HttpException extends Error {
   public status: number;
   public message: string;
 
-  constructor(status: number, message: string) {
+  constructor(status: HttpStatus, message: string) {
     super(message);
     this.status = status;
     this.message = message;
