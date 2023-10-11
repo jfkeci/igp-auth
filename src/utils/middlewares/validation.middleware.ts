fdcace076f312d9e5e30037c54f079fbd03d086b
jfkeci <ja.lip132@gmail.com>
2023-10-11 08:32:31 +0200

    feat: validation middleware

---
+++ src/utils/middlewares/validation.middleware.ts
@@ -0,0 +1,31 @@
+import Joi from 'joi';
+import { Request, Response, NextFunction, RequestHandler } from 'express';
+
+export const ValidationMiddleware = (schema: Joi.Schema): RequestHandler => {
+  return async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    const validationOptions = {
+      abortEarly: false,
+      allowUnknown: true,
+      stripUnknown: true,
+    };
+
+    try {
+      const value = await schema.validateAsync(req.body, validationOptions);
+      req.body = value;
+      next();
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (e: any) {
+      const errors: string[] = [];
+
+      e.details.forEach((error: Joi.ValidationErrorItem) => {
+        errors.push(error.message);
+      });
+
+      res.status(400).send({ errors: errors });
+    }
+  };
+};
