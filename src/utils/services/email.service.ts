1efa99ff7bda304a75934c63943eb50fa37376e2
jfkeci <ja.lip132@gmail.com>
2023-10-11 08:48:01 +0200

    feat: email service

---
+++ src/utils/services/email.service.ts
@@ -0,0 +1,40 @@
+import nodemailer, { SendMailOptions, Transporter } from 'nodemailer';
+import { logger } from '../logger';
+
+export class EmailService {
+  constructor() {}
+
+  private async createTestAccout(): Promise<nodemailer.TestAccount> {
+    return await nodemailer.createTestAccount();
+  }
+
+  private createTransporter(creds: nodemailer.TestAccount): Transporter {
+    const smtp = {
+      user: creds.user,
+      pass: creds.pass,
+      host: creds.smtp.host,
+      port: creds.smtp.port,
+      secure: false,
+    };
+
+    return nodemailer.createTransport({
+      ...smtp,
+      auth: { user: smtp.user, pass: smtp.pass },
+    });
+  }
+
+  async sendEmail(payload: SendMailOptions): Promise<void> {
+    const creds = await this.createTestAccout();
+
+    const transporter = this.createTransporter(creds);
+
+    transporter.sendMail(payload, (err, info) => {
+      if (err) {
+        logger.error(`${err.message}, Error sending email`);
+        return;
+      }
+
+      logger.info(`Preview URL: ${nodemailer.getTestMessageUrl(info)}`);
+    });
+  }
+}
