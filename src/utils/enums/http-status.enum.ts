92c4a7ae67ce44abb4915bbd59c02ce99ffd36a8
jfkeci <ja.lip132@gmail.com>
2023-10-11 09:12:55 +0200

    chore: rename http status enum

--- src/utils/enums/http-status.enum.ts
+++ src/utils/enums/http-status.enum.ts
@@ -1,4 +1,4 @@
-export enum HttpStatusCode {
+export enum HttpStatus {
   /**
    * The server has received the request headers and the client should proceed to send the request body
    * (in the case of a request for which a body needs to be sent; for example, a POST request).
