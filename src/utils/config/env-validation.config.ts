3199ee9b0433d05aa8387ef57f93812498c07b16
jfkeci <ja.lip132@gmail.com>
2023-10-11 08:51:27 +0200

    chore: add NODE_ENV to env validation config service

--- src/utils/config/env-validation.config.ts
+++ src/utils/config/env-validation.config.ts
@@ -3,6 +3,9 @@ import Joi from 'joi';
 export class ConfigValidationService {
   private envValidationConfig = Joi.object({
     PORT: Joi.number().default(3000),
+    NODE_ENV: Joi.string()
+      .valid('development', 'production')
+      .default('development'),
     DATABASE_URL: Joi.string().required(),
     API_KEY: Joi.string().required(),
   }).unknown();
