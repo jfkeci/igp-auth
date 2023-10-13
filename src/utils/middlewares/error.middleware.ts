a50a97bbf2693eb96d0b16c787b80ed746254908
jfkeci <ja.lip132@gmail.com>
2023-10-13 12:17:34 +0200

    feat: check error type, handle mongoose errors in error middleware

--- src/utils/middlewares/error.middleware.ts
+++ src/utils/middlewares/error.middleware.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import { Request, Response, NextFunction } from 'express';
 import { HttpException } from '../classes/http-exception.class';
 
@@ -9,6 +10,18 @@ export const ErrorMiddleware = (
   _next: NextFunction,
 ): void => {
   const status = error instanceof HttpException ? error.status : 500;
+
+  if (error instanceof mongoose.Error.ValidationError) {
+    res.status(400).json({ error: error.message });
+  }
+
+  if (error.status === 11000) {
+    res.status(409).json({ status: 409, error: 'Duplicate key error' });
+  }
+
+  if (error instanceof mongoose.Error.CastError) {
+    res.status(400).json({ error: 'Invalid ID format' });
+  }
 
   const message = error.message || 'Something went wrong';
 
