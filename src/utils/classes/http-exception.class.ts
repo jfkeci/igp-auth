cc903d9381894e63abfd49999e61d22b466199e0
jfkeci <ja.lip132@gmail.com>
2023-10-13 10:45:06 +0200

    feat: add optional error type property to http exception class

--- src/utils/classes/http-exception.class.ts
+++ src/utils/classes/http-exception.class.ts
@@ -1,11 +1,15 @@
+import { ErrorType } from '../enums/error-type.enum';
 import { HttpStatus } from '../enums/http-status.enum';
 
 export class HttpException extends Error {
   public status: number;
   public message: string;
 
-  constructor(status: HttpStatus, message: string) {
+  public type?: ErrorType;
+
+  constructor(status: HttpStatus, message: string, type?: ErrorType) {
     super(message);
+    this.type = type;
     this.status = status;
     this.message = message;
   }
