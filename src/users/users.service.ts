192ff2ef14d4a888e4d82191bdb209afffbf7c97
jfkeci <ja.lip132@gmail.com>
2023-10-13 12:04:59 +0200

    feat: add wrapper methods to users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,13 +3,16 @@ import User from './interfaces/user.interface';
 import { HttpStatus } from '../utils/enums/http-status.enum';
 import { HttpException } from '../utils/classes/http-exception.class';
 import { CreateUserParams } from './interfaces/create-user-params.interface';
+import { ObjectId, FilterQuery, QueryOptions, ProjectionType } from 'mongoose';
 
-export class UserService extends UserRepo {
+export class UserService {
+  private usersRepo = new UserRepo();
+
   async createOne(data: CreateUserParams): Promise<User> {
     let user: User;
 
     try {
-      user = await this._createOne(data);
+      user = await this.usersRepo._createOne(data);
     } catch (error) {
       throw new HttpException(
         HttpStatus.BAD_REQUEST,
@@ -25,7 +28,7 @@ export class UserService extends UserRepo {
   }
 
   async findByEmail(email: string): Promise<User> {
-    const user = await this._findOne({ email });
+    const user = await this.usersRepo._findOne({ email });
 
     if (!user) {
       throw new HttpException(HttpStatus.NOT_FOUND, 'User not found');
@@ -35,12 +38,36 @@ export class UserService extends UserRepo {
   }
 
   async findByUsername(username: string): Promise<User> {
-    const user = await this._findOne({ username });
+    const user = await this.usersRepo._findOne({ username });
 
     if (!user) {
       throw new HttpException(HttpStatus.NOT_FOUND, 'User not found');
     }
 
     return user.toObject();
+  }
+
+  async _findOne(
+    filter?: FilterQuery<User>,
+    projection?: ProjectionType<User> | null,
+    options?: QueryOptions<User> | null,
+  ) {
+    return await this.usersRepo._findOne(filter, projection, options);
+  }
+
+  async _findById(
+    id: string | ObjectId,
+    projection?: ProjectionType<User> | null,
+    options?: QueryOptions<User> | null,
+  ) {
+    return await this.usersRepo._findById(id, projection, options);
+  }
+
+  async _find(
+    filter?: FilterQuery<User>,
+    projection?: ProjectionType<User> | null,
+    options?: QueryOptions<User> | null,
+  ): Promise<User[]> {
+    return await this.usersRepo._find(filter ?? {}, projection, options);
   }
 }
