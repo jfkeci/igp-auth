2d8d7903be5ea0b95521e16a6321edd2a28193a9
jfkeci <ja.lip132@gmail.com>
2023-10-13 12:10:47 +0200

    chore: auth controller and routes login and register endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,18 @@
-import { Router } from 'express';
-import Controller from '../utils/interfaces/controller.interface';
 import { AuthRoutes } from './auth.routes';
+import { AuthService } from './auth.service';
+import User from '../users/interfaces/user.interface';
+import { HttpStatus } from '../utils/enums/http-status.enum';
+import { ResponseType } from '../utils/enums/response-type.enum';
+import { NextFunction, Request, Response, Router } from 'express';
+import Controller from '../utils/interfaces/controller.interface';
+import { LoginUserParams } from './interfaces/login-user-params.interface';
+import { ResponseFormat } from '../utils/interfaces/response-format.interface';
+import { RegisterUserParams } from './interfaces/register-user-params.interface';
 
 export class AuthController implements Controller {
   public router = Router();
+
+  private authService = new AuthService();
 
   constructor() {
     this.initRoutes();
@@ -11,5 +20,57 @@ export class AuthController implements Controller {
 
   private initRoutes(): void {
     this.router = new AuthRoutes().init(this);
+  }
+
+  async loginUser(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    const { email, password } = req.body;
+
+    try {
+      const result = await this.authService.loginUser({
+        email,
+        password,
+      } as LoginUserParams);
+
+      const response: ResponseFormat<User> = {
+        type: ResponseType.User,
+        status: HttpStatus.OK,
+        data: result,
+      };
+
+      res.status(HttpStatus.OK).json(response);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async registerUser(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    const { username, email, password, confirmPassword } = req.body;
+
+    try {
+      const result = await this.authService.registerUser({
+        confirmPassword,
+        password,
+        username,
+        email,
+      } as RegisterUserParams);
+
+      const response: ResponseFormat<User> = {
+        type: ResponseType.User,
+        status: HttpStatus.CREATED,
+        data: result,
+      };
+
+      res.status(HttpStatus.CREATED).json(response);
+    } catch (error) {
+      next(error);
+    }
   }
 }
--- src/auth/auth.routes.ts
+++ src/auth/auth.routes.ts
@@ -1,17 +1,41 @@
+import {
+  loginValidationSchema,
+  registerValidationSchema,
+} from './auth.validation';
 import { Router } from 'express';
 import { AuthController } from './auth.controller';
+import { ValidationMiddleware } from '../utils/middlewares/validation.middleware';
+import { logger } from '../utils/logger';
+import { ConfigService } from '../utils/config/env.config';
 
 export class AuthRoutes {
   private router = Router();
   private path = '/auth';
 
+  private config = new ConfigService();
+
   public init(controller: AuthController): Router {
+    const apiPrefix = this.config.get<string>('prefix');
+
     // [ ] Login any user
-    /* this.router.post(
+    logger.info(
+      `${controller.constructor.name} POST ${apiPrefix}${this.path}/login route`,
+    );
+    this.router.post(
       `${this.path}/login`,
-      [ValidationMiddleware(validate.login)],
-      controller.login,
-    ); */
+      [ValidationMiddleware(loginValidationSchema)],
+      controller.loginUser.bind(controller),
+    );
+
+    // [ ] Register user
+    logger.info(
+      `${controller.constructor.name} POST ${apiPrefix}${this.path}/register route`,
+    );
+    this.router.post(
+      `${this.path}/register`,
+      [ValidationMiddleware(registerValidationSchema)],
+      controller.registerUser.bind(controller),
+    );
 
     // [ ] Verify user
 
