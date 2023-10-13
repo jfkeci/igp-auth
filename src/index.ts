d337c5abaa68128e84f1144243913391bd777345
jfkeci <ja.lip132@gmail.com>
2023-10-13 12:16:56 +0200

    feat: add user and notifications controller to the app

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,20 @@ import dotenv from 'dotenv';
 import { App } from './app';
 import { AuthController } from './auth/auth.controller';
 import { ConfigValidationService } from './utils/config/env-validation.config';
+import { UserNotificationsController } from './user-notifications/user-notifications.controller';
+import { UsersController } from './users/users.controller';
 
 dotenv.config();
 
 new ConfigValidationService().validateEnv();
 
-const app = new App([new AuthController()], Number(process.env.API_PORT));
+const app = new App(
+  [
+    new AuthController(),
+    new UsersController(),
+    new UserNotificationsController(),
+  ],
+  Number(process.env.API_PORT),
+);
 
 app.listen();
