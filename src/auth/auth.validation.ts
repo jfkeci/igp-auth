ac3ddbdb65d268df9111e12c7a2ca0ad171752cf
jfkeci <ja.lip132@gmail.com>
2023-10-13 12:09:54 +0200

    feat: add custom validation messages to login and registration joi validation schemas

--- src/auth/auth.validation.ts
+++ src/auth/auth.validation.ts
@@ -7,7 +7,16 @@ export const registerValidationSchema = Joi.object({
   confirmPassword: Joi.string().required().valid(Joi.ref('password')),
 });
 
+/** Error messages are vague for a reason */
 export const loginValidationSchema = Joi.object({
-  email: Joi.string().email().required().min(3).max(255),
-  password: Joi.string().min(6).max(255).required(),
+  email: Joi.string().email().required().min(3).max(255).messages({
+    'string.email': 'Invalid email',
+    'any.required': 'Email Required',
+    'min.max': 'Invalid email',
+  }),
+  password: Joi.string().min(6).max(255).required().messages({
+    string: 'Invalid password',
+    'any.required': 'Password Required',
+    'min.max': 'Invalid password',
+  }),
 });
