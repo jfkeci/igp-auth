fb003cd4e3d553c0a87437ba17d8c91f39f4f62c
jfkeci <ja.lip132@gmail.com>
2023-10-14 17:29:23 +0200

    feat: serve static pages only in development

--- src/static/static.rotues.ts
+++ src/static/static.rotues.ts
@@ -10,16 +10,20 @@ export class StaticRoutes {
   private config = new ConfigService();
 
   public init(controller: StaticController): Router {
+    const apiEnv = this.config.get<string>('env');
     const apiPrefix = this.config.get<string>('prefix');
 
-    // [ ] Serve confirm email pages
-    logger.info(
-      `${controller.constructor.name} GET ${apiPrefix}${this.path}/confirm-email route`,
-    );
-    this.router.get(
-      `${this.path}/confirm-email`,
-      controller.getConfirmEmailPage.bind(controller),
-    );
+    if (apiEnv === 'development') {
+      // [x] Serve confirm email pages
+      logger.info(
+        `${controller.constructor.name} GET ${apiPrefix}${this.path}/confirm-email route`,
+      );
+
+      this.router.get(
+        `${this.path}/confirm-email`,
+        controller.getConfirmEmailPage.bind(controller),
+      );
+    }
 
     return this.router;
   }
