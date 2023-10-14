1629ae28a90d140288d34729617c1b154d6c3a63
jfkeci <ja.lip132@gmail.com>
2023-10-14 17:32:21 +0200

    chore: add return types to AuthMiddleware

--- src/utils/middlewares/auth.middleware.ts
+++ src/utils/middlewares/auth.middleware.ts
@@ -1,34 +1,36 @@
-import { NextFunction } from 'express';
 import { JwtService } from '../services/jwt.service';
 import { HttpStatus } from '../enums/http-status.enum';
 import { UserService } from '../../users/users.service';
 import { HttpException } from '../classes/http-exception.class';
+import { Request, Response, NextFunction, RequestHandler } from 'express';
 
-export const AuthMiddleware = async (
-  req: Request,
-  _: Response,
-  next: NextFunction,
-): Promise<Response | void> => {
-  const jwtService = new JwtService();
-  const userService = new UserService();
+export const AuthMiddleware = (): RequestHandler => {
+  return async (
+    req: Request,
+    _: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    const jwtService = new JwtService();
+    const userService = new UserService();
 
-  const token = jwtService.getTokenFromHeaders(req);
+    const token = jwtService.getTokenFromHeaders(req);
 
-  if (!token) {
-    return next(new HttpException(HttpStatus.UNAUTHORIZED, 'Unauthorised'));
-  }
+    if (!token) {
+      return next(new HttpException(HttpStatus.UNAUTHORIZED, 'Unauthorised'));
+    }
 
-  const id = jwtService.verify(token);
+    const id = jwtService.verify(token);
 
-  if (!id) {
-    return next(new HttpException(HttpStatus.UNAUTHORIZED, 'Unauthorised'));
-  }
+    if (!id) {
+      return next(new HttpException(HttpStatus.UNAUTHORIZED, 'Unauthorised'));
+    }
 
-  const user = userService._findById(id);
+    const user = userService._findById(id);
 
-  if (!user) {
-    return next(new HttpException(HttpStatus.UNAUTHORIZED, 'Unauthorised'));
-  }
+    if (!user) {
+      return next(new HttpException(HttpStatus.UNAUTHORIZED, 'Unauthorised'));
+    }
 
-  next();
+    next();
+  };
 };
