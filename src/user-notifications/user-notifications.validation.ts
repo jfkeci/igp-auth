8f327b0fdcbbcd632d9c859dd613d81e831cd7e1
jfkeci <ja.lip132@gmail.com>
2023-10-15 16:49:29 +0200

    chore: add createNotificationValidationSchema

--- src/user-notifications/user-notifications.validation.ts
+++ src/user-notifications/user-notifications.validation.ts
@@ -1,7 +1,12 @@
 import Joi from 'joi';
 
 export const createUserNotificationValidationSchema = Joi.object({
-  title: Joi.string().max(3).max(150),
+  title: Joi.string().max(3).max(150).required(),
+  body: Joi.string().optional().min(3).max(250),
+});
+
+export const createNotificationValidationSchema = Joi.object({
+  title: Joi.string().max(3).max(150).required(),
   body: Joi.string().optional().min(3).max(250),
   userId: Joi.string().required(),
 });
