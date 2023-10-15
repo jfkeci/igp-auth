79ba627dc20ae0d5d67ff9914a2299954d841ee9
jfkeci <ja.lip132@gmail.com>
2023-10-15 16:46:15 +0200

    feat: disable log rotation via env variable

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -9,27 +9,35 @@ const colors = {
   http: 'magenta',
 };
 
-export const logger = winston.createLogger({
-  level: process.env.LOG_LEVEL,
-  format: winston.format.combine(
-    winston.format.timestamp({ format: 'dd-mm-YYYY hh:MM:ss' }),
-    winston.format.errors({ stack: true }),
-    winston.format.colorize({ colors }),
-    winston.format.printf(({ timestamp, level, message, label, stack }) => {
-      return `${timestamp} [${label || 'iGP Auth'}] ${level}: ${message}${
-        stack ? '\n' + stack : ''
-      }`;
-    }),
-  ),
-  transports: [
-    new winston.transports.Console(),
-    new DailyRotateFile({
-      dirname: 'logs',
-      filename: 'application-%DATE%.log',
-      datePattern: 'YYYY-MM-DD',
-      zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '14d',
-    }),
-  ],
-});
+const loggerConfig = () => {
+  const transports = JSON.parse(process.env.LOG_ROTATION_ENABLED ?? 'false')
+    ? [
+        new winston.transports.Console(),
+        new DailyRotateFile({
+          dirname: 'logs',
+          filename: 'application-%DATE%.log',
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: true,
+          maxSize: '20m',
+          maxFiles: '14d',
+        }),
+      ]
+    : [new winston.transports.Console()];
+
+  return winston.createLogger({
+    transports,
+    level: process.env.LOG_LEVEL,
+    format: winston.format.combine(
+      winston.format.timestamp({ format: 'dd-mm-YYYY hh:MM:ss' }),
+      winston.format.errors({ stack: true }),
+      winston.format.colorize({ colors }),
+      winston.format.printf(({ timestamp, level, message, label, stack }) => {
+        return `${timestamp} [${label || 'iGP Auth'}] ${level}: ${message}${
+          stack ? '\n' + stack : ''
+        }`;
+      }),
+    ),
+  });
+};
+
+export const logger = loggerConfig();
